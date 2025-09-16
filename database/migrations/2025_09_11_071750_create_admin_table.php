c7be4beaa0f16c32e74a87dcbc80fdaf893fbee7
issam@helloce.fr <elabadli.issam@gmail.com>
2025-09-16 11:31:18 +0200

    Update 2025_09_11_071750_create_admin_table.php

--- database/migrations/2025_09_11_071750_create_admin_table.php
+++ database/migrations/2025_09_11_071750_create_admin_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('admin', function (Blueprint $table) {
             $table->id();
             $table->string('email')->unique();
-            $table->string('password');;
+            $table->string('password');
             $table->timestamps();
         });
     }
